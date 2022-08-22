ed38d32fe834fe699fa4b7043d96262bd563d8b5
chouaib Mounaime <chouaib.mounaime@diabeloop.fr>
2022-08-23 00:07:03 +0200

    app: handle search queries in search screen

--- app/src/main/java/com/example/foodiez/ui/search/SearchScreen.kt
+++ app/src/main/java/com/example/foodiez/ui/search/SearchScreen.kt
@@ -3,9 +3,20 @@ package com.example.foodiez.ui.search
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIos
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -13,17 +24,34 @@ import com.example.foodiez.navigation.Screen
 import com.example.foodiez.ui.common.MealCard
 import com.example.foodiez.ui.product.ProductSource
 import com.example.foodiez.ui.theme.CreamWhite2
+import com.example.foodiez.ui.theme.Gray
+import com.example.foodiez.ui.theme.Green100
 
 @Composable
 fun SearchScreen(navController: NavController, viewModel: SearchViewModel = hiltViewModel()) {
     val products = viewModel.productsList.collectAsState().value
 
-    Box(
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp),
         modifier = Modifier
             .background(CreamWhite2)
             .fillMaxSize()
-            .padding(horizontal = 16.dp)
+            .padding(start = 16.dp, end = 16.dp, top = 16.dp)
     ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(onClick = { navController.popBackStack() }) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBackIos,
+                    contentDescription = "back",
+                    tint = Green100
+                )
+            }
+
+            SearchBar(onValueChanged = { viewModel.searchRequested(it) })
+        }
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(10.dp),
             modifier = Modifier.fillMaxHeight()
@@ -45,3 +73,65 @@ fun SearchScreen(navController: NavController, viewModel: SearchViewModel = hilt
         }
     }
 }
+
+
+@Composable
+fun SearchBar(onValueChanged: ((String) -> Unit)) {
+    var state by remember { mutableStateOf(TextFieldValue("")) }
+    Surface(
+        elevation = 3.dp,
+        shape = RoundedCornerShape(8.dp),
+    ) {
+        BasicTextField(
+            value = state,
+            onValueChange = {
+                state = it
+                onValueChanged(it.text)
+            },
+            singleLine = true,
+            maxLines = 1,
+            modifier = Modifier
+                .background(Gray, RoundedCornerShape(8.dp))
+                .height(45.dp)
+                .fillMaxWidth(),
+            decorationBox = { innerTextField ->
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(10.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(start = 10.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "search icon",
+                        tint = Green100
+                    )
+                    Box(
+                        modifier = Modifier.weight(1f),
+                        contentAlignment = Alignment.CenterStart
+                    ) {
+                        if (state == TextFieldValue(""))
+                            Text("Search...")
+                        innerTextField()
+                    }
+                    if (state != TextFieldValue("")) {
+                        IconButton(
+                            onClick = { state = TextFieldValue("") },
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Close,
+                                contentDescription = "reset search bar",
+                                tint = Green100
+                            )
+                        }
+                    }
+                }
+            }
+        )
+    }
+}
+
+//@Preview()
+//@Composable
+//fun SearchBarPreview() {
+//   SearchBar()
+//}
--- app/src/main/java/com/example/foodiez/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/foodiez/ui/search/SearchViewModel.kt
@@ -1,11 +1,15 @@
 package com.example.foodiez.ui.search
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.foodiez.data.utils.Constants.Companion.TAG
 import com.example.foodiez.domain.product.Product
 import com.example.foodiez.domain.product.ProductRepository
 import com.example.foodiez.domain.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -16,9 +20,20 @@ class SearchViewModel @Inject constructor(private val productRepository: Product
 
     var productsList: MutableStateFlow<List<Product>> = MutableStateFlow(value = emptyList())
 
-    init {
+    private var searchJob : Job? = null
+
+    fun searchRequested(query: String) {
+        searchJob?.cancel()
+        searchJob = viewModelScope.launch {
+            delay(500)
+            searchByQuery(query)
+        }
+    }
+
+    fun searchByQuery(query: String) {
+        Log.e(TAG, "searchByQuery: $query", )
         viewModelScope.launch {
-            productRepository.searchByQuery("whey").collectLatest { result ->
+            productRepository.searchByQuery(query).collectLatest { result ->
                 when (result) {
                     is Resource.Success -> productsList.value = result.data ?: emptyList()
                     is Resource.Error -> productsList.value = emptyList()

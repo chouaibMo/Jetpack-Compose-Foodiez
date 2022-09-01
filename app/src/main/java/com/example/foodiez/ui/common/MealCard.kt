0b514342358330c4d20ac1fdea80f911091f6a1c
chouaib Mounaime <chouaib.mounaime@diabeloop.fr>
2022-09-01 23:35:58 +0200

    app: fix meal card on delete swipe bug

---
+++ app/src/main/java/com/example/foodiez/ui/common/MealCard.kt
@@ -0,0 +1,104 @@
+package com.example.foodiez.ui.common
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
+import com.example.foodiez.domain.product.Product
+import com.example.foodiez.ui.theme.Gray
+import com.example.foodiez.ui.theme.Red
+import de.charlex.compose.RevealDirection
+import de.charlex.compose.RevealSwipe
+import de.charlex.compose.RevealValue
+import de.charlex.compose.rememberRevealState
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun SwipeableMealCard(product: Product, onClick: () -> Unit, onSwipe: () -> Unit) {
+    val revealState = rememberRevealState()
+    val scope = rememberCoroutineScope()
+
+    RevealSwipe(
+        state = revealState,
+        shape = RoundedCornerShape(8.dp),
+        directions = setOf(RevealDirection.EndToStart),
+        closeOnBackgroundClick = true,
+        backgroundCardEndColor = Red,
+        hiddenContentEnd = {
+            IconButton(onClick = {
+                scope.launch {
+                    revealState.animateTo(RevealValue.Default)
+                }
+                onSwipe()
+            }) {
+                Icon(
+                    imageVector = Icons.Outlined.Delete,
+                    contentDescription = "delete product",
+                    tint = Color.White,
+                    modifier = Modifier.padding(horizontal = 25.dp)
+                )
+            }
+        },
+    ) {
+        MealCard(product, onClick)
+    }
+}
+
+@Composable
+fun MealCard(product: Product, onClick: (() -> Unit)) {
+    Card(
+        elevation = 5.dp,
+        backgroundColor = Gray,
+        shape = RoundedCornerShape(10.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .clickable { onClick() },
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = rememberImagePainter(product.imageURL),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(50.dp)
+                    .padding(start = 8.dp, top = 4.dp, bottom = 4.dp)
+                    .clip(RoundedCornerShape(10.dp))
+            )
+            Column(Modifier.padding(8.dp)) {
+                Text(
+                    maxLines = 1,
+                    text = product.name,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+                    Text(
+                        text = "${
+                            product.caloriesByQuantity().toInt()
+                        } kcal • ${product.quantity} g/ml",
+                        color = Color.Gray,
+                        fontSize = 12.sp
+                    )
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/foodiez/ui/common/MealsList.kt
+++ app/src/main/java/com/example/foodiez/ui/common/MealsList.kt
@@ -1,35 +1,23 @@
 package com.example.foodiez.ui.common
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import coil.compose.rememberImagePainter
 import com.example.foodiez.domain.product.MealType
 import com.example.foodiez.domain.product.Product
 import com.example.foodiez.navigation.Screen
 import com.example.foodiez.ui.home.HomeViewModel
 import com.example.foodiez.ui.product.ProductSource
 import com.example.foodiez.ui.theme.Dark
-import com.example.foodiez.ui.theme.Gray
-import com.example.foodiez.ui.theme.Red
-import de.charlex.compose.RevealDirection
-import de.charlex.compose.RevealSwipe
 
 @Composable
 fun MealsList(navController: NavController, mealType: MealType, products: List<Product>, viewModel: HomeViewModel) {
@@ -44,11 +32,7 @@ fun MealsList(navController: NavController, mealType: MealType, products: List<P
                     .fillMaxWidth()
                     .padding(start = 4.dp, end = 4.dp, bottom = 6.dp)
             ) {
-                Text(
-                    text = mealType.tag,
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.ExtraBold
-                )
+                Text(text = mealType.tag, fontSize = 20.sp, fontWeight = FontWeight.ExtraBold)
                 Icon(
                     imageVector = Icons.Default.AddCircle,
                     contentDescription = "Add product",
@@ -58,10 +42,10 @@ fun MealsList(navController: NavController, mealType: MealType, products: List<P
             }
         }
         Column(
+            verticalArrangement = Arrangement.spacedBy(12.dp),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(bottom = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
             filteredProducts.forEach {
                 SwipeableMealCard(
@@ -78,72 +62,6 @@ fun MealsList(navController: NavController, mealType: MealType, products: List<P
                         }
                     }
                 )
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun SwipeableMealCard(product: Product, onClick: () -> Unit, onSwipe: () -> Unit) {
-    RevealSwipe(
-        shape = RoundedCornerShape(8.dp),
-        directions = setOf(RevealDirection.EndToStart),
-        closeOnBackgroundClick = true,
-        backgroundCardEndColor = Red,
-        hiddenContentEnd = {
-            IconButton(onClick = { onSwipe() }) {
-                Icon(
-                    imageVector = Icons.Outlined.Delete,
-                    contentDescription = "delete product",
-                    tint = Color.White,
-                    modifier = Modifier.padding(horizontal = 25.dp)
-                )
-            }
-        }
-    ) {
-        MealCard(product, onClick)
-    }
-}
-
-@Composable
-fun MealCard(product: Product, onClick: (() -> Unit)) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .clickable { onClick() },
-        shape = RoundedCornerShape(10.dp),
-        elevation = 5.dp,
-        backgroundColor = Gray
-    ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Image(
-                painter = rememberImagePainter(product.imageURL),
-                contentDescription = "",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(50.dp)
-                    .padding(start = 8.dp, top = 4.dp, bottom = 4.dp)
-                    .clip(RoundedCornerShape(10.dp))
-            )
-            Column(Modifier.padding(8.dp)) {
-                Text(
-                    maxLines = 1,
-                    text = product.name,
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.Bold,
-                    overflow = TextOverflow.Ellipsis
-                )
-                Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
-                    Text(
-                        text = "${
-                            product.caloriesByQuantity().toInt()
-                        } kcal • ${product.quantity} g/ml",
-                        color = Color.Gray,
-                        fontSize = 12.sp
-                    )
-                }
             }
         }
     }

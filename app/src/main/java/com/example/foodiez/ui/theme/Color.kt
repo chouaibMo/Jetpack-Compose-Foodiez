5008744df55bb2d328b2414e09ec80e50a201603
chouaib Mounaime <chouaib.mounaime@diabeloop.fr>
2022-09-15 22:20:43 +0200

    app: dark mode colors and theme

--- app/src/main/java/com/example/foodiez/ui/theme/Color.kt
+++ app/src/main/java/com/example/foodiez/ui/theme/Color.kt
@@ -2,15 +2,18 @@ package com.example.foodiez.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-
+// light mode colors
+val BackgroundCreamWhite = Color(0xFFFFFFF0)
+val SurfaceCreamWhite = Color(0xFFEEEADD)
 val Dark = Color(0xFF3C3C3C)
-val Gray = Color(0xFFEEEADD)
-val Green = Color(0xFF86D773)
 
+
+// dark mode colors
+val BackgroundDark = Color(0xFF2F2F2F)
+val SurfaceDark = Color(0xFF3C3C3C)
+
+
+// common colors
 val Green100 = Color(0xFF57B940)
 val Green50 = Color(0x5986D773)
 val Blue100 = Color(0xFF45aaf2)
@@ -18,5 +21,5 @@ val Blue50 = Color(0x5945AAF2)
 val Orange100 = Color(0xFFfa8231)
 val Orange50 = Color(0x59FA8231)
 
-val CreamWhite2 = Color(0xFFFFFFF0)
+val Green = Color(0xFF86D773)
 val Red = Color(0xFFf44949)
--- app/src/main/java/com/example/foodiez/ui/theme/Theme.kt
+++ app/src/main/java/com/example/foodiez/ui/theme/Theme.kt
@@ -5,26 +5,23 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Green,
+    background = BackgroundDark,
+    surface = SurfaceDark,
+    onBackground = Color.White,
+    onSurface = Color.White,
+
 )
 
 private val LightColorPalette = lightColors(
     primary = Green,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    background = BackgroundCreamWhite,
+    surface = SurfaceCreamWhite,
+    onBackground = Dark,
+    onSurface = Dark
 )
 
 @Composable
